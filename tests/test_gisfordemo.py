b2afd8b30649ae9579216c1229e8f0b7d0ad149b
ChangwhaOh <coh4@vols.utk.edu>
2023-02-23 10:02:58 -0500

    for creating package

---
+++ tests/test_gisfordemo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `gisfordemo` package."""
+
+
+import unittest
+
+from gisfordemo import gisfordemo
+
+
+class TestGisfordemo(unittest.TestCase):
+    """Tests for `gisfordemo` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
