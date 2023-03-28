75315ebb915ad35450219dbdff1b950f1728911e
ChangwhaOh <coh4@vols.utk.edu>
2023-03-28 11:00:27 -0400

    Minor changes

--- gisfordemo/gisfordemo.py
+++ gisfordemo/gisfordemo.py
@@ -4,7 +4,17 @@ import random
 import string
 
 def generate_random_string(length, upper = False, digit = False, punc = False):
-    """Generates a random string of a specified length."""
+    """Generates a random string of a given length.
+
+    Args:
+        length (int): _description_
+        upper (bool, optional): _description_. Defaults to False.
+        digit (bool, optional): _description_. Defaults to False.
+        punc (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        str: _description_
+    """
     chars = string.ascii_lowercase
     if upper:
         chars += string.ascii_uppercase
@@ -17,6 +27,14 @@ def generate_random_string(length, upper = False, digit = False, punc = False):
     return result_str
 
 def generate_lucky_number(length = 1):
+    """_summary_
+
+    Args:
+        length (int, optional): _description_. Defaults to 1.
+
+    Returns:
+        int: _description_
+    """    
     result_str = ''.join(random.choice(string.digits) for i in range(length))
     result_str = int(result_str)
     return result_str
