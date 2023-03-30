b9b36897f8516445009d295f23192300f0977e77
ChangwhaOh <coh4@vols.utk.edu>
2023-03-30 10:59:33 -0400

    Added ipyleaflet

--- gisfordemo/gisfordemo.py
+++ gisfordemo/gisfordemo.py
@@ -2,6 +2,40 @@
 
 import random
 import string
+import ipyleaflet
+
+
+class Map(ipyleaflet.Map):
+
+    def __init__(self, center, zoom, **kwargs):
+        """_summary_
+
+        Args:
+            center (_type_): _description_
+            zoom (_type_): _description_
+        """        
+        if 'scroll_wheel_zoom' not in kwargs:
+            kwargs['scroll_wheel_zoom'] = True
+        super().__init__(center = center, zoom = zoom, **kwargs) # inherited from the parent, in this case, ipyleaflet
+    
+    def add_search_control(self, position = 'topleft', **kwargs):
+        """_summary_
+
+        Args:
+            position (str, optional): _description_. Defaults to 'topleft'.
+        """        
+        if 'url' not in kwargs:
+            kwargs['url'] = 'https://nominatim.openstreetmap.org/search?format=json&q={s}'
+        
+        search_control = ipyleaflet.SearchControl(position = position, **kwargs)
+        self.add_control(search_control)
+
+    def add_draw_control(self, **kwargs):
+        draw_control = ipyleaflet.DrawControl(**kwargs)
+        self.add_control(draw_control)
+
+
+
 
 def generate_random_string(length, upper = False, digit = False, punc = False):
     """Generates a random string of a given length.
